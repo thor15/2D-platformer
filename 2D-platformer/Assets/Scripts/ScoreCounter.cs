f0a7db261d40e7ea47d57644c698de0ebca0ff35
AriesWarrior888 <74877205+AriesWarrior888@users.noreply.github.com>
2020-11-24 14:58:26 -0600

    Score Code
    But its broken

---
+++ 2D-platformer/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 10,000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = score - 10;
+    }
+}
