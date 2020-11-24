80da58c99b692fda5f4bce868b603e2956c48684
thor15 <thor15@users.noreply.github.com>
2020-11-24 14:22:18 -0600

    Created portal and folder for scripts

---
+++ 2D-platformer/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    private Rigidbody personRB;
+    private bool isOnGround;
+    private Vector3 offest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        personRB = GetComponent<Rigidbody>();
+        isOnGround = true;
+        offest = mainCamera.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * 3 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * 3 * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.W) && isOnGround)
+        {
+            personRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            isOnGround = false;
+        }
+        mainCamera.transform.position = transform.position + offest;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
