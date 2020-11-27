1be92db67717f1fb94f02f5af8d34cb97e7bec1b
thor15 <thor15@users.noreply.github.com>
2020-11-27 16:36:33 -0600

    Made switching between levels possible
    Also made it so that if the player fell past y = -10 then they would be reset to the position (0,0,0), finally the score would rest when switching between levels

---
+++ 2D-platformer/Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerController : MonoBehaviour
+{
+    public MovementScript player;
+    public ScoreCounter counter;
+    public GameObject level1;
+    public GameObject level2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) && player.canEndLevel)
+        {
+            
+            counter.count = false;
+            counter.gameObject.SetActive(false);
+            level1.SetActive(false);
+            level2.SetActive(true);
+            player.gameObject.transform.position = new Vector3(0, 0, 0);
+            counter.gameObject.SetActive(true);
+
+        }
+
+        if(player.transform.position.y < -10)
+        {
+            player.transform.position = new Vector3(0, 0, 0);
+        }
+
+
+    }
+}
