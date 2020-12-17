0efd0aea0623cac70a071c77fea0a83f06e5d9b4
AriesWarrior888 <74877205+AriesWarrior888@users.noreply.github.com>
2020-12-17 15:20:20 -0600

    Made Classes
    Made Classes

---
+++ 2D-platformer/Assets/Scripts/Level.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level
+{
+    public Ground grounds = new Ground();
+    public Coin coins = new Coin();
+}
