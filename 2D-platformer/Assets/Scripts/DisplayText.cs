fff57e68f736e06d81576d2e5b4e774a2d0e2cd7
AriesWarrior888 <74877205+AriesWarrior888@users.noreply.github.com>
2020-12-19 15:37:15 -0600

    Not working code
    made first text box but everything is broken

---
+++ 2D-platformer/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class DisplayText : MonoBehaviour
+{
+
+    public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDisable()
+    {
+        text.gameObject.SetActive(true);
+        StartCoroutine(timeToDisable());
+    }
+
+    IEnumerator timeToDisable()
+    {
+        yield return new WaitForSeconds(10);
+        text.gameObject.SetActive(false);
+    }
+}
