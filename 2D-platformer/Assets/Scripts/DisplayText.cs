088a2883421799603ff91622dc390c264cc86968
thor15 <thor15@users.noreply.github.com>
2020-12-21 15:36:34 -0600

    Added more levels
    Created 2 more levels

--- 2D-platformer/Assets/Scripts/DisplayText.cs
+++ 2D-platformer/Assets/Scripts/DisplayText.cs
@@ -50,21 +50,6 @@ public class DisplayText : MonoBehaviour
         yield return new WaitForSeconds(5);
         Debug.LogError("Text is False");
         text.gameObject.SetActive(false);
-        Destroy(this);
+        Destroy(this.gameObject);
     }
-
-
-    IEnumerator ExampleCoroutine()
-    {
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-
-        //yield on a new YieldInstruction that waits for 5 seconds.
-        yield return new WaitForSeconds(5);
-
-        //After we have waited 5 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-    }
-
-
 }
