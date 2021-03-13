fe5e57292c893238e5154bbf2f5e9fc61ed08faa
thor15 <thor15@users.noreply.github.com>
2021-03-13 15:56:08 -0600

    Created a Background
    Has a plane that changes to a colored background

---
+++ 2D-platformer/Assets/Scripts/Background/TileGeneration.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TerrainType
+{
+	public string name;
+	public float height;
+	public Color color;
+}
+
+public class TileGeneration : MonoBehaviour
+{
+	[SerializeField]
+	private TerrainType[] terrainTypes;
+
+	[SerializeField]
+	NoiseMapGeneration noiseMapGeneration;
+
+	[SerializeField]
+	private MeshRenderer tileRenderer;
+
+	[SerializeField]
+	private MeshFilter meshFilter;
+
+	[SerializeField]
+	private MeshCollider meshCollider;
+
+	[SerializeField]
+	private float mapScale;
+
+	void Start()
+	{
+		GenerateTile();
+	}
+
+	void GenerateTile()
+	{
+		// calculate tile depth and width based on the mesh vertices
+		Vector3[] meshVertices = this.meshFilter.mesh.vertices;
+		int tileDepth = (int)Mathf.Sqrt(meshVertices.Length);
+		int tileWidth = tileDepth;
+
+		// calculate the offsets based on the tile position
+		float[,] heightMap = this.noiseMapGeneration.GenerateNoiseMap(tileDepth, tileWidth, this.mapScale);
+
+		// generate a heightMap using noise
+		Texture2D tileTexture = BuildTexture(heightMap);
+		this.tileRenderer.material.mainTexture = tileTexture;
+	}
+	
+	private Texture2D BuildTexture(float[,] heightMap)
+	{
+		int tileDepth = heightMap.GetLength(0);
+		int tileWidth = heightMap.GetLength(1);
+
+		Color[] colorMap = new Color[tileDepth * tileWidth];
+		for (int zIndex = 0; zIndex < tileDepth; zIndex++)
+		{
+			for (int xIndex = 0; xIndex < tileWidth; xIndex++)
+			{
+				// transform the 2D map index is an Array index
+				int colorIndex = zIndex * tileWidth + xIndex;
+				float height = heightMap[zIndex, xIndex];
+				// choose a terrain type according to the height value
+				TerrainType terrainType = ChooseTerrainType(height);
+				// assign the color according to the terrain type
+				colorMap[colorIndex] = terrainType.color;
+			}
+		}
+
+		// create a new texture and set its pixel colors
+		Texture2D tileTexture = new Texture2D(tileWidth, tileDepth);
+		tileTexture.wrapMode = TextureWrapMode.Clamp;
+		tileTexture.SetPixels(colorMap);
+		tileTexture.Apply();
+
+		return tileTexture;
+	}
+
+
+	TerrainType ChooseTerrainType(float height)
+	{
+		// for each terrain type, check if the height is lower than the one for the terrain type
+		foreach (TerrainType terrainType in terrainTypes)
+		{
+			// return the first terrain type whose height is higher than the generated one
+			if (height < terrainType.height)
+			{
+				return terrainType;
+			}
+		}
+		return terrainTypes[terrainTypes.Length - 1];
+	}
+
+}
